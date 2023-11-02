5d9fda73387752e66395d643aabcd7724ad15bc5
richard <hannemanrichard7@gmail.com>
2023-11-02 22:48:48 +0100

    add pixels

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,6 +20,18 @@ export default function App({ Component, pageProps }: AppProps) {
         });
       });
   }, [router.events]);
+  useEffect(() => {
+    import("react-facebook-pixel")
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init("274446805028105"); // facebookPixelId
+        ReactPixel.pageView();
+
+        router.events.on("routeChangeComplete", () => {
+          ReactPixel.pageView();
+        });
+      });
+  }, [router.events]);
 
   return (
     <>
