e8c8bd622b8577d7704acde3100318cbb7e6b4d9
richard <hannemanrichard7@gmail.com>
2024-02-07 04:00:02 +0100

    second commit

--- pages/thankyou.tsx
+++ pages/thankyou.tsx
@@ -18,26 +18,27 @@ export default function ThankYou() {
         />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <div className="bg-auto bg-no-repeat bg-[#151515] bg-center min-h-screen">
-        <header className=" bg-[#151515] border-b border-gray-600 fixed top-0 h-20 w-full">
+      <div className="bg-auto bg-no-repeat bg-center">
+        <header className="bg-white border-b border-gray-300 fixed top-0 h-20 w-full z-20">
           <div className="w-full flex justify-between px-3 py-3">
             <div className="py-3">
               <img src="logo.png" className="h-8" alt="" />
             </div>
-            <div className="mt-3">
-              <Link href="/">
-                <a className=" bg-orange-500 text-white px-6 py-3 rounded-lg font-bold">
-                  أطلب الآن
-                </a>
-              </Link>
+            <div className=" mt-3">
+              <a
+                href="#form"
+                className=" bg-amber-600 hover:bg-amber-500 text-white duration-150 ease-in-out  px-6 py-3 rounded-lg font-bold"
+              >
+                أطلب الآن
+              </a>
             </div>
           </div>
         </header>
-        <main className="w-full bg-[#151515] mt-20 px-6">
+        <main className="w-full  mt-20 px-6">
           <div className="w-full pt-4 pb-8 text-center  z-10 ">
             <div className="pt-20 w-2/3 mx-auto">
-              <h1 className="text-5xl font-bold text-white">تهانينا</h1>
-              <h6 className="text-2xl text-white">
+              <h1 className="text-5xl font-bold ">تهانينا</h1>
+              <h6 className="text-2xl ">
                 شكرا جزيلا على ثقتكم سيتم الإتصال بكم في غضون 24 ساعة لتأكيد
                 طلبكم فالرجاء إبقاء الهاتف مفتوح{" "}
               </h6>
